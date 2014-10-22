e2a6f7f51dae62f9f29249277c5803f72c45d4fa
patrick <patrick@debian>
2014-10-21 21:42:31 -0400

    The location of the stack is now algorithmic instead of being based on the end of the kernel. To decide the location of the stack I search for the first region of high memory, make sure I'm not looking at memory used by the kernel, go to the next 4K aligned address (because it seemed like a good idea), make sure I have enough size in the region, and put my stack there.

--- src/c/memory_manager.c
+++ src/c/memory_manager.c
@@ -9,8 +9,8 @@ static u32int kernel_end = (u32int) &end;
 
 // variables used to define a stack.
 u32int *stack_low;
-u32int *stack_ptr;
-u32int *stack_high;
+u32int *stack_ptr = 0;
+u32int *stack_high; // stack high getting set to 0 is being used as a flag value.
 
 void push_physical_address(u32int addr)
 {
@@ -22,6 +22,13 @@ void push_physical_address(u32int addr)
 		vga_buffer_put_str("\nHalting.");
 		for (;;) {}
 	}
+	
+	/*
+	vga_buffer_put_str("\nPush ");
+	vga_buffer_put_hex(addr);
+	vga_buffer_put_str(" stack_ptr=");
+	vga_buffer_put_hex((u32int) stack_ptr);
+	*/
 	
 	stack_ptr--;
 	*stack_ptr = addr;
@@ -37,6 +44,8 @@ u32int pop_physical_address()
 		vga_buffer_put_str("\nHalting.");
 		for (;;) {}
 	}
+	
+	
 	u32int addr = *stack_ptr;
 	stack_ptr++;
 	return addr;
@@ -101,13 +110,13 @@ void memory_manager_initialize(struct multiboot *mboot_ptr)
 	vga_buffer_put_str(" mmap_length=");
 	vga_buffer_put_hex(mmap.length);
 	
-	vga_buffer_put_str("\n");
-	vga_buffer_put_dec(mem_in_mb);
-	vga_buffer_put_str("MB of memory provides ");
-	// figure how how much memory i have in kilobytes.
+	//vga_buffer_put_str("\n");
+	//vga_buffer_put_dec(mem_in_mb);
+	//vga_buffer_put_str("MB of memory provides ");
+	// figure how how much total memory i have in kilobytes.
 	u32int mem_in_kb = mem_in_mb * 1024;
-	vga_buffer_put_dec(mem_in_kb);
-	vga_buffer_put_str("KB.");
+	//vga_buffer_put_dec(mem_in_kb);
+	//vga_buffer_put_str("KB.");
 	
 	// how many 4KB blocks of memory will i have?
 	u32int stack_size = mem_in_kb / 4;
@@ -115,22 +124,83 @@ void memory_manager_initialize(struct multiboot *mboot_ptr)
 	vga_buffer_put_dec(stack_size);
 	vga_buffer_put_str(" entries to store every 4096th address.");
 	
-	// Assuming that I'll always have enough space right after my kernel may be bad.
-	// i might need to loop through the memory map and find an area that's explicitly large enough for the stack.
-	
-	stack_low = (u32int *) (kernel_end + 1);
-	vga_buffer_put_str("\nstack_low=");
-	vga_buffer_put_hex((u32int) stack_low);
-	
-	stack_high = (u32int *) (stack_low + stack_size);
-	vga_buffer_put_str(" stack_high=");
-	vga_buffer_put_hex((u32int) stack_high);
-	
-	stack_ptr = stack_high;
-	vga_buffer_put_str(" stack_ptr=");
-	vga_buffer_put_hex((u32int) stack_ptr);
-	
+	vga_buffer_put_str("\nFinding space for the stack.");
 	u32int i = mmap.addr;
+	while (i < (mmap.addr + mmap.length))
+	{
+		u32int *size = (u32int *) i;
+		u32int *base_addr = (u32int *) (i + 4);
+		u32int *length = (u32int *) (i + 12);
+		u32int *type = (u32int *) (i + 20);
+		
+		if ((*type == 1) && (*base_addr >= 0x100000))
+		{
+			vga_buffer_put_str("\nRegion start: ");
+			vga_buffer_put_hex(*base_addr);
+			vga_buffer_put_str(" Length: ");
+			vga_buffer_put_hex(*length); // the length is the size of the region in kilobytes
+			
+			// i should get the finishing address of the region, maybe?
+			
+			u32int end_addr = (*base_addr + *length) - 1;
+			vga_buffer_put_str(" Last address: ");
+			vga_buffer_put_hex(end_addr);
+			
+			// is the address inside the kernel?
+			vga_buffer_put_str("\nMaking sure base_addr doesn't point to memory used by the kernel.");
+			if (*base_addr >= kernel_start && *base_addr < kernel_end)
+			{
+				while ((*base_addr >= kernel_start && *base_addr < kernel_end))
+				{
+					*base_addr = *base_addr + 1;
+				}
+			}
+			
+			// make sure the address is page aligned.
+			// don't know if i need this, but it may come in handy.
+			if (*base_addr & 0xFFF)
+			{
+				*base_addr &= ~(0xFFF);
+				*base_addr = *base_addr + 0x1000;
+			}
+			
+			vga_buffer_put_str("\nbase_addr is now ");
+			vga_buffer_put_hex(*base_addr);
+			
+			// make sure there's enough space for the stack.
+			vga_buffer_put_str("\nSpace available: ");
+			vga_buffer_put_dec(end_addr - *base_addr);
+			
+			vga_buffer_put_str(" Needed: ");
+			vga_buffer_put_dec((stack_size * sizeof(u32int)));
+			
+			if ((end_addr - *base_addr) > (stack_size * sizeof(u32int)))
+			{
+				// there's enough space
+				vga_buffer_put_str("\nI have enough space.");
+				
+				stack_low = (u32int *) *base_addr;
+				vga_buffer_put_str("\nstack_low=");
+				vga_buffer_put_hex((u32int) stack_low);
+				
+				stack_high = (u32int *) (stack_low + stack_size);
+				vga_buffer_put_str(" stack_high=");
+				vga_buffer_put_hex((u32int) stack_high);
+				
+				stack_ptr = stack_high;
+				vga_buffer_put_str(" stack_ptr=");
+				vga_buffer_put_hex((u32int) stack_ptr);
+				
+				// i'm done
+				break;
+			}
+			
+		}
+		
+		i += *size + 4;
+	}
+		
+	i = mmap.addr;
 	while (i < (mmap.addr + mmap.length))
 	{
 		u32int *size = (u32int *) i;
@@ -157,7 +227,15 @@ void memory_manager_initialize(struct multiboot *mboot_ptr)
 			if (*base_addr >= 0x100000)
 			{
 				vga_buffer_put_str("\nRegion is in upper memory.");
-				for (u32int j = *base_addr; j < (*base_addr + *length); j += 4096)
+				u32int j = *base_addr;
+				// make sure the address is 4K aligned
+				if (j & 0xFFF)
+				{
+					j &= ~(0xFFF); // set the last twelve bits of the pointer to 0 (this causes the pointer to back up!)
+					j = j + 0x1000; // advance the pointer, don't back it up.
+				}
+				
+				for (; j < (*base_addr + *length); j += 4096)
 				{
 					if (j >= (u32int) stack_low && j < (u32int) stack_high)
 					{
@@ -167,13 +245,6 @@ void memory_manager_initialize(struct multiboot *mboot_ptr)
 					{
 						continue;
 					}
-					
-					// make sure the address is 4K aligned
-					if (j & 0xFFF)
-					{
-						j &= ~(0xFFF);
-					}
-					
 					push_physical_address(j);
 				}
 				vga_buffer_put_str("\nRegion addresses pushed to stack. stack_ptr=");
