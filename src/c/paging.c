3fe093b19c709fbc9e31d2328bdff55bd003db2c
patrick <patrick@debian>
2015-02-25 22:06:48 -0500

    I think I have the page directory being made correctly. I think I have the first page table being made correctly. I have the address of the first page table being put on the first index of the page directory array.

--- src/c/paging.c
+++ src/c/paging.c
@@ -82,10 +82,10 @@ void paging_initialize(struct multiboot *mboot_ptr)
 	put_dec(tot_pages);
 	put_str(" pages of memory to use.");
 	
+	u32int num_page_tables = tot_pages / 1024;
+	
 	put_str("\nI'll end up with ");
-	put_dec(tot_pages);
-	put_str(" / 1024 = ");
-	put_dec(tot_pages / 1024);
+	put_dec(num_page_tables);
 	put_str(" page tables in my page directory.");
 	
 	put_str("\nKernel starts at ");
@@ -112,21 +112,131 @@ void paging_initialize(struct multiboot *mboot_ptr)
 	// i need to clear out the 4KB of memory where the page directory address starts
 	memset((u8int *) page_directory, 0, 4096);
 	
+	put_str("\nAddress of page_directory[0] is ");
+	put_hex((u32int) &page_directory[0]);
+	
+	put_str("\nAddress of page_directory[1023] is ");
+	put_hex((u32int) &page_directory[1023]);
+	
 	// i need to create a blank page directory
+	// this gives me 1024 entries on the page table that are 0x00000002
 	for (int i = 0; i < 1024; i++)
 	{
 		page_directory[i] = 0 | 2;
 	}
+	
+	// i need to create my page tables.
+	/*
+		
+		CR3 = page directory address, 109000 (or the calculated address for the page directory + 0x1000
+		
+		structure of page directory
+		 
+		page_directory[0] = address to first page table (10A000)
+		page_directory[1] = 10B000
+		page_directory[2] = 10C000
+		...
+		page_directory[31] = address for 32nd page table
+		page_directory[32] = 0 | 2
+		...
+		page_directory[1023] = 0 | 2
+		
+		structure for the page tables
+		
+		I'll have 32 page tables, each will be 4kb in size.
+		memory will look like (assuming 128MB of memory):
+		
+		0x0 - 0xFFFFF = low memory (1MB used)
+		
+		0x100000 - 0x108D54 = kernel
+		0x108D55 - 0x108FFF = space that's wasted to bump up to the next page aligned address (36KB used)
+		
+		0x109000 - 0x109FFF = page directory (4KB used)
+		
+		0x10A000 - 0x10AFFF = first page table
+		0x10B000 - 0x10BFFF = second page table
+		...
+		0x? - 0x?FFF = 1024th page table (4MB used)
+		
+		Page tables should start around the 40KB + 5MB mark, around 0x50A000
+		table_one[0] = 0x0
+		table_one[1] = 0x1000
+		table_one[2] = 0x2000
+		...
+		table_one[1023] = 0x900000? somewhere around there.
+		
+	 */
 	
 	// i need to create tot_pages / 1024 page tables, and put them in the page directory.
 	// 1024 is the number of entries i can have on a page table
 	// with 128MB of memory i should have 32 page tables
 	// this means i'll have 32 entries on my page directory when i'm done.
 	
+	
+	
+	
+	
+	
+	
+	// attempting to create the first page table
+	// this page table will cover the first 4KB of memory
+	// this will be from address 0x0 to 0x1000
+	
+	u32int *page_table = (u32int *) ((u32int) &page_directory[1023] + 0x4);
+	
+	put_str("\nThe address of the first page table is ");
+	put_hex((u32int) page_table);
+	
+	u32int addr_counter = 0x0;
+	u32int dir_counter = 0;
+	// while (or for?) loop here to create multiple page tables
+	for (int i = 0; i < 1024; i++)
+	{
+		page_table[i] = addr_counter;
+		addr_counter = addr_counter + 0x1000;
+	}
+	
+	//put_str("\nValue of page_table[0] is ");
+	//put_hex(page_table[0]);
+	
+	//put_str("\nValue of page_table[1] is ");
+	//put_hex(page_table[1]);
+	
+	//put_str("\nValue of page_table[2] is ");
+	//put_hex(page_table[2]);
+	
+	//put_str("\nValue of page_table[1021] is ");
+	//put_hex(page_table[1021]);
+	
+	//put_str("\nValue of page_table[1022] is ");
+	//put_hex(page_table[1022]);
+	
+	put_str("\nAddress of page_table[1023] is ");
+	put_hex((u32int) &page_table[1023]);
+	
+	put_str("\nValue of page_table[1023] is ");
+	put_hex(page_table[1023]);
+	
+	
+	// i should now put the page table address in page_directory[0]
+	page_directory[dir_counter] = (u32int) page_table;
+	
+	put_str("\nValue of page_directory[0] is ");
+	put_hex(page_directory[0]);
+	
+	// end loop here
+	
+	
+	
+	
+	
+	
+	
+	
 	put_str("\nDone initializing paging. Hope it worked!\n"); // remove when done
 }
 
-void page_fault_interrupt_handler(registers regs)
+void page_fault_interrupt_handler(__attribute__ ((unused)) registers regs)
 {
 	
 }
