66d51fa9928ae5c420e8ba1029e31dba2e94578f
patrick <patrick@debian>
2015-03-09 15:31:48 -0400

    Cleaned out the paging stuff for another redo.

--- src/h/paging.h
+++ src/h/paging.h
@@ -10,12 +10,6 @@ typedef struct memory_map_struct
 	u32int length;
 } __attribute__ ((packed)) memory_map;
 
-// funtions i'll need in assembly
-
-// functions i'll need to manipulate cr3 (read and write)
-// functions i'll need to manipulate cr2 (read and write)
-// functions i'll need to manipulate cr0 (read)
-
 extern u32int read_cr0();
 extern void write_cr0(u32int);
 
@@ -27,17 +21,6 @@ extern void write_cr3(u32int);
 
 extern u32int read_cr4();
 extern void write_cr4(u32int);
-
-void paging_print_info();
-
-// paging stack initilzation function.
-void paging_stack_initialize();
-
-// functions i need to push and pop from the paging stack.
-void paging_stack_push(u32int phys_addr);
-u32int paging_stack_pop();
-u32int paging_stack_full();
-u32int paging_stack_empty();
 
 // paging initialization function
 void paging_initialize(struct multiboot *mboot_ptr);
